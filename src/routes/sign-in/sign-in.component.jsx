f5d48646f1e41472253c08b33aeed2f798714ee2
Adewale Ayokanmi Adeleye <adewagold@gmail.com>
2022-08-15 23:13:15 +0100

    Added signing utils and added firebase utils and firebase library

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,6 +1,20 @@
+import { Fragment } from "react";
+import { signiInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+
 const Signin = ()=>{
+    const logGoogleUser = async () =>{
+        const {user} = await signiInWithGooglePopup();
+        const userDocRef = await createUserDocumentFromAuth(user);
+    }
+
     return (
-        <h1>Sign in</h1>
+        <Fragment>
+            <h1>Sign in</h1>
+            <button onClick={logGoogleUser}>
+            Sign in with google poppup
+            </button>
+        </Fragment>
+        
     )
 }
 
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,61 @@
+import {initializeApp} from 'firebase/app'
+import {getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider} from 'firebase/auth'
+import {getFirestore, doc, getDoc, setDoc} from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBnNrvFPNMaljJHn-hgJNto2ba3x3hxNLc",
+    authDomain: "awesomecommerce-62cdf.firebaseapp.com",
+    projectId: "awesomecommerce-62cdf",
+    storageBucket: "awesomecommerce-62cdf.appspot.com",
+    messagingSenderId: "1075170676368",
+    appId: "1:1075170676368:web:ababae8dacededd1e05215"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  //provider
+  const provider = new GoogleAuthProvider();
+  provider.setCustomParameters({
+      prompt: "select_account"
+  })
+
+
+export const auth  = getAuth(); 
+export const signiInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) =>  {
+    //create a new document with a unique id in the db
+    const userDocRef = doc(db, 'users', userAuth.uid)
+    console.log(userDocRef);
+
+    //check the snapshot of the document
+    const userSnapshot = await getDoc(userDocRef);
+    console.log(userSnapshot);
+    //Check if the reference exists
+    console.log(userSnapshot.exists())
+
+    if(!userSnapshot.exists()){
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        try{
+            await setDoc(userDocRef, {
+                displayName, email, createdAt
+            })
+        }
+        catch(error){
+            console.log('error creating the user', error);
+        }
+    }
+    return userDocRef;
+    //If user data does not exists
+    //create / set the document with the data from userAuth in my collection
+    //Chec if user data exists
+
+    //return userDocReference
+    
+}
